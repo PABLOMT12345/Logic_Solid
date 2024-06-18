abc9d897e164ac390df7e986cee4199f8d3b56a2
Sofia-Nav <157772830+Sofia-Nav@users.noreply.github.com>
2024-06-18 07:04:47 +0000

    se agrego el archivo de solid

--- solid.py
+++ solid.py
@@ -1,9 +1,9 @@
-class Coche {  
-    String marca;
-
-    Coche(String marca){ this.marca = marca; }
-
-    String getMarcaCoche(){ return marca; }
-
-    void guardarCocheDB(Coche coche){ ... }
-}
+class Discount:
+  def _init_(self, customer, price):
+      self.customer = customer
+      self.price = price
+  def give_discount(self):
+      if self.customer == 'fav':
+          return self.price * 0.2
+      if self.customer == 'vip':
+          return self.price * 0.4
